911a3f62a155942f13cfa8740b21ad8a772b388d
Bryan Burnside <bpburnside@gmail.com>
2023-06-01 20:18:54 -0500

    Add edit/save functionality to Story

--- client/components/Story.jsx
+++ client/components/Story.jsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import 'bulma/css/bulma.min.css';
 
 const Story = () => {
   const [prompt, setPrompt] = useState('');
   const [story, setStory] = useState([]);
+  const [editedStory, setEditedStory] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [title, setTitle] = useState('');
 
   const handleInput = (e) => {
     const { value } = e.target;
-    console.log(value);
     setPrompt(value);
   }
 
@@ -15,17 +18,64 @@ const Story = () => {
     try {
       const response = await axios.post('/api/stories', { prompt });
       const story = response.data.choices[0].text;
-      setStory(story.split('\n'));
+      setTitle(story.split(':')[0].trim());
+      const paragraphs = story.split(':')[1].split('\n');
+      setStory(paragraphs);
+      setEditedStory(paragraphs);
     } catch (err) {
       console.error('Failed to POST story to API at client:', err);
     }
   }
 
+  const handleEditToggle = () => {
+    setIsEditing(prevState => !prevState);
+    setStory(editedStory);
+    setTitle(title);
+  }
+
+  const handleSaveToggle = () => {
+    setIsEditing(prevState => !prevState);
+    setStory(editedStory);
+    setTitle(title);
+    saveStory();
+  }
+
+  const saveStory = async () => {
+    try {
+      await axios.post('/api/stories/:id', {story, title});
+    } catch (err) {
+      console.error('Failed to SAVE story to DB at client:', err)
+    }
+  }
+
+  const handleEdit = (e) => {
+    const { value } = e.target;
+    setEditedStory(value.split('\n'));
+  }
+
   return (
     <div>
-      <input onChange={handleInput}></input>
+      <input onChange={handleInput} />
       <button onClick={handleSubmit}>Submit Prompt</button>
-      {story.map((paragraph, i) => <p key={i}>{ paragraph }</p>)}
+      <p align="center">{title}</p>
+      {isEditing ? (
+        <div>
+          <textarea
+          value={editedStory.join('\n')}
+          onChange={handleEdit}
+          rows={10}
+          cols={100}
+          />
+          <button onClick={handleSaveToggle}>Save</button>
+        </div>
+      ) : (
+        <div>
+          {story.map((paragraph, i) => (
+            <p key={i}>{paragraph}</p>
+          ))}
+          <button onClick={handleEditToggle}>Edit</button>
+        </div>
+      )}
     </div>
   );
 }
