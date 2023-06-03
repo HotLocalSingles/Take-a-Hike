b1bbb6845998a29f0a2a6a3ad8bfbeb9e9b7af24
Bryan Burnside <bpburnside@gmail.com>
2023-06-03 17:58:20 -0500

    CRUD complete, component styled, complete

--- client/components/Story.jsx
+++ client/components/Story.jsx
@@ -2,29 +2,26 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import 'bulma/css/bulma.min.css';
 
-const Story = () => {
+const Story = ( { id }) => {
   const [prompt, setPrompt] = useState('');
   const [story, setStory] = useState([]);
   const [editedStory, setEditedStory] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [title, setTitle] = useState('');
-  const [user, setUser] = useState(null);
+  const [userStories, setUserStories] = useState([]);
 
-  const getUserObj = () => {
-    axios.get("/user").then((response) => {
-      const user = response.data;
-      console.log(user);
-      setUser(user);
-    })
-    .catch((err) => {
-      console.error("Could not GET user at client", err);
-      // navigate('/login');
-    })
-  }
+  const getUserStories = async () => {
+    try {
+      const response = await axios.get(`/api/stories/${id}`);
+      setUserStories(response.data);
+    } catch (err) {
+      console.error('Failed to GET stories from DB at client:', err);
+    }
+  };
 
   useEffect (() => {
-    getUserObj();
-  }, [])
+    getUserStories();
+  }, []);
 
   const handleInput = (e) => {
     const { value } = e.target;
@@ -55,13 +52,14 @@ const Story = () => {
     setStory(editedStory);
     setTitle(title);
     saveStory();
+    getUserStories();
   }
 
   const saveStory = async () => {
     try {
-      await axios.post(`/api/stories/${user._id}`, {story, title});
+      await axios.put(`/api/stories/${id}`, {story: editedStory, title});
     } catch (err) {
-      console.error('Failed to SAVE story to DB at client:', err)
+      console.error('Failed to SAVE story to DB at client:', err);
     }
   }
 
@@ -70,30 +68,90 @@ const Story = () => {
     setEditedStory(value.split('\n'));
   }
 
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`/api/stories/${id}/${title}`);
+      setTitle('');
+      setStory([])
+      getUserStories();
+    } catch (err) {
+      setTitle('');
+      setStory([]);
+      console.error('Failed to DELETE story at client:', err);
+    }
+  };
+
+  const getStory = (e) => {
+    const { innerText } = e.target;
+    setTitle(innerText);
+    const clickedStory = userStories.map(story => innerText === story.title ? story.story : null)
+    setStory(clickedStory);
+    setEditedStory(clickedStory);
+  }
+
   return (
-    <div>
-      <input onChange={handleInput} />
-      <button onClick={handleSubmit}>Submit Prompt</button>
-      <p align="center">{title}</p>
-      {isEditing ? (
-        <div>
-          <textarea
-          value={editedStory.join('\n')}
-          onChange={handleEdit}
-          rows={10}
-          cols={100}
-          />
-          <button onClick={handleSaveToggle}>Save</button>
+    <>
+      <div className="split-screen" style={{ display: "flex", maxWidth: '800px' }}>
+        <div className="block">
+        <div className="left-panel"
+          style={{
+            flex: "0 0 40%",
+            background: "#f1f1f1",
+            padding: "20px",
+          }}>
+            {userStories.map((story, i) => (
+            <p
+            style={{
+              cursor: "pointer",
+              fontStyle: "italic"
+             }}
+            onClick={getStory}
+            key={i}
+            >
+              {story.title}
+            </p>
+            ))}
+            </div>
+            <div className="block" style={{ display: "flex", alignItems: "center" }}>
+              <input className="input is-small" type="text" placeholder="Enter a story prompt!" onChange={handleInput} />
+              <button style={{marginLeft: '3px'}} className="button is-info is-small" onClick={handleSubmit}>Submit</button>
+            </div>
         </div>
-      ) : (
-        <div>
-          {story.map((paragraph, i) => (
-            <p key={i}>{paragraph}</p>
-          ))}
-          <button onClick={handleEditToggle}>Edit</button>
+        <div
+          className="right-panel"
+          style={{
+            flex: "1",
+            padding: "20px",
+          }}
+        >
+          <p align="center">{title}</p>
+          {isEditing ? (
+          <div>
+            <textarea
+            className="textarea"
+            value={editedStory.join('')}
+            onChange={handleEdit}
+            rows={10}
+            cols={100}
+            />
+            <div style={{ textAlign: "right" }}>
+              <button className="button is-primary is-small" onClick={handleSaveToggle} style={{ marginTop: '10px' }}>Save</button>
+            </div>
+          </div>
+          ) : (
+          <div>
+            {story.map((paragraph, i) => (
+              <p key={i}>{paragraph}</p>
+            ))}
+            <div style={{ textAlign: "right" }}>
+              {story.length > 0 ? <button className="button is-warning is-small" onClick={handleEditToggle}>Edit</button> : null}
+              {story.length > 0 ? <button className="button is-danger is-small" onClick={handleDelete} style={{ marginLeft: '10px' }}>Delete</button> : null}
+            </div>
+          </div>
+           )}
         </div>
-      )}
-    </div>
+      </div>
+    </>
   );
 }
 
