21b89a943523deacbef3b8fcc5bba6a95b301331
Bryan Burnside <bpburnside@gmail.com>
2023-06-03 17:59:04 -0500

    Add complete CRUD functionality, complete

--- server/database/routes/storyRouter.js
+++ server/database/routes/storyRouter.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 require('dotenv').config();
-const { saveStory } = require('../models/stories.js')
-
+const { Stories, saveStory } = require('../models/stories.js');
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
@@ -23,17 +22,17 @@ const getStory = async (word) => {
   return response.data;
 }
 
-router.post('/:id', async (req, res) => {
-  const { title, story } = req.body;
-  const { id } = req.params;
-  try {
-    await saveStory(title, story.join(''), id);
-    res.sendStatus(201);
-  } catch (err) {
-    console.error('Failed to SAVE story to DB at server:', err);
-    res.sendStatus(500);
-  }
-});
+// router.post('/:id', async (req, res) => {
+//   const { title, story } = req.body;
+//   const { id } = req.params;
+//   try {
+//     await saveStory(title, story.join(''), id);
+//     res.sendStatus(201);
+//   } catch (err) {
+//     console.error('Failed to SAVE story to DB at server:', err);
+//     res.sendStatus(500);
+//   }
+// });
 
 router.post('/', async (req, res) => {
   const { prompt } = req.body;
@@ -42,6 +41,54 @@ router.post('/', async (req, res) => {
     res.status(201).send(story);
   } catch (err) {
     console.error('Failed to POST prompt to API at server:', err);
+    res.sendStatus(500);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const stories = await Stories.findAll({ where: { user_id: id } });
+    console.log(stories)
+    res.status(200).send(stories);
+  } catch (err) {
+    console.error('Failed to GET stories from DB:', err);
+    res.sendStatus(500);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  const { title, story } = req.body;
+  const { id } = req.params;
+  try {
+    const storyDB = await Stories.findOne({ where: {user_id: id, title}});
+    if (storyDB) {
+      storyDB.update({ story: story.join('') });
+      res.sendStatus(204);
+    } else {
+      await saveStory(title, story.join(''), id);
+      res.sendStatus(201);
+    }
+  } catch (err) {
+    console.error('Failed to UPDATE story to DB at server:', err);
+    res.sendStatus(500);
+  }
+});
+
+router.delete('/:id/:title', async (req, res) => {
+  const { id, title } = req.params;
+  console.log(title);
+  try {
+    const storyDB = await Stories.findOne({ where: {user_id: id, title}});
+    if (storyDB) {
+      await storyDB.destroy();
+      res.sendStatus(200);
+    } else {
+      console.error('Failed to find user story at DB');
+      res.sendStatus(404);
+    }
+  } catch (err) {
+    console.error('Failed to DELETE story to DB at server:', err);
     res.sendStatus(500);
   }
 });
